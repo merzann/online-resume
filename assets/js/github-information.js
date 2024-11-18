eb43f1273ea10a3f8ee2fa9a1826282877d8c976
merzann <annisch78@gmail.com>
2024-11-18 21:10:59 +0000

    feat: add function to fetch user-information from Github and send response to HTML, add function to handle user-not-found-error and other status errors

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,4 @@
+// function to handle user input and send request to github
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
@@ -6,8 +7,30 @@ function fetchGitHubInformation(event) {
         return;
     }
 
+    // display loader
     $("#gh-user-data").html(
         `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
+
+    // function to handle response from github and display response in gh-user-data
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        },
+        // function to handle user-not-found-response
+        // display result in console and to user
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
