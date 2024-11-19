e0d1dc3fe3e12ae727d523780298000348352219
merzann <annisch78@gmail.com>
2024-11-19 00:19:26 +0000

    Fix bugs for the gh-user-data & gh-repo elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -40,6 +40,8 @@ function repoInformationHTML(repos) {
 
 // function to process user input and send request to github
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -77,3 +79,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
