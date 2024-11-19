f57170d0bfb2ddba1dcf597b19b123e5484b1461
merzann <annisch78@gmail.com>
2024-11-19 23:55:39 +0000

    feat: implement emailjs integration on contact page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,18 @@
-console.log("hello!");
+// function to process user input in form
+function sendMail(contactForm) {
+    emailjs.send("service_ffyg3bn", "template_10wsfda", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.projectsummary.value,
+        "reply_to": contactForm.emailaddress.value,
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
