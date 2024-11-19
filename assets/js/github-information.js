4f0b42022675e451c5485f5a35ea0351895659bf
merzann <annisch78@gmail.com>
2024-11-19 00:04:33 +0000

    fix: amend style.css and github-information.js to fix returned repos not correctly displayed

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,4 +1,44 @@
-// function to handle user input and send request to github
+// function to display user information
+function userInformationHTML(user) {
+    return `
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
+
+// function to display repo information
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
+}
+
+// function to process user input and send request to github
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
@@ -7,20 +47,23 @@ function fetchGitHubInformation(event) {
         return;
     }
 
-    // display loader
     $("#gh-user-data").html(
         `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
-    // function to handle response from github and display response in gh-user-data
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(response) {
-            var userData = response;
+        // function to process returned repository data
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
+
         // function to handle user-not-found-response
         // display result in console and to user
         function(errorResponse) {
